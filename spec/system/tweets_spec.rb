08b177cbf9c87597e09c728b23872d0b36c728c3
yout-a <rainyday1388@gmail.com>
2025-09-14 13:27:05 +0900

    削除・詳細のテスト

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -105,3 +105,101 @@ RSpec.describe 'ツイート編集', type: :system do
     end
   end
 end
+
+RSpec.describe 'ツイート削除', type: :system do
+  before do
+    @tweet1 = FactoryBot.create(:tweet)
+    @tweet2 = FactoryBot.create(:tweet)
+  end
+    context 'ツイート削除ができるとき' do
+    it 'ログインしたユーザーは自らが投稿したツイートの削除ができる' do
+      # ツイート1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'Email', with: @tweet1.user.email
+      fill_in 'Password', with: @tweet1.user.password
+      find('input[name="commit"]').click
+      expect(page).to have_current_path(root_path)
+      # ツイート1に「削除」へのリンクがあることを確認する
+      expect(
+        all('.more')[1].hover
+      ).to have_link '削除', href: tweet_path(@tweet1)
+      # 投稿を削除するとレコードの数が1減ることを確認する
+      expect{
+        all('.more')[1].hover.find_link('削除', href: tweet_path(@tweet1)).click
+        sleep 1
+      }.to change { Tweet.count }.by(-1)
+      # トップページにはツイート1の内容が存在しないことを確認する（画像）
+      expect(page).to have_no_selector ".content_post[style='background-image: url(#{@tweet1.image});']"
+      # トップページにはツイート1の内容が存在しないことを確認する（テキスト）
+      expect(page).to have_no_content("#{@tweet1.text}")
+    end
+  end
+  context 'ツイート削除ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿したツイートの削除ができない' do
+      # ツイート1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'Email', with: @tweet1.user.email
+      fill_in 'Password', with: @tweet1.user.password
+      find('input[name="commit"]').click
+      expect(page).to have_current_path(root_path)
+      # ツイート2に「削除」へのリンクがないことを確認する
+      expect(
+        all('.more')[0].hover
+      ).to have_no_link '削除', href: tweet_path(@tweet2)
+    end
+    it 'ログインしていないとツイートの削除ボタンがない' do
+      # トップページに移動する
+      visit root_path
+      # ツイート1に「削除」へのリンクがないことを確認する
+      expect(
+        all('.more')[1].hover
+      ).to have_no_link '削除', href: tweet_path(@tweet1)
+      # ツイート2に「削除」へのリンクがないことを確認する
+      expect(
+        all('.more')[0].hover
+      ).to have_no_link '削除', href: tweet_path(@tweet2)
+    end
+  end
+end
+
+RSpec.describe 'ツイート詳細', type: :system do
+  before do
+    @tweet = FactoryBot.create(:tweet)
+  end
+  it 'ログインしたユーザーはツイート詳細ページに遷移してコメント投稿欄が表示される' do
+    # ログインする
+    visit new_user_session_path
+    fill_in 'Email', with: @tweet.user.email
+    fill_in 'Password', with: @tweet.user.password
+    find('input[name="commit"]').click
+    expect(page).to have_current_path(root_path)
+    # ツイートに「詳細」へのリンクがある
+    expect(
+      all(".more")[0].hover
+    ).to have_link '詳細', href: tweet_path(@tweet)
+    # 詳細ページに遷移する
+    visit tweet_path(@tweet)
+    # 詳細ページにツイートの内容が含まれている
+    expect(page).to have_selector ".content_post[style='background-image: url(#{@tweet.image});']"
+    expect(page).to have_content("#{@tweet.text}")
+    # コメント用のフォームが存在する
+    expect(page).to have_selector 'form'
+  end
+  it 'ログインしていない状態でツイート詳細ページに遷移できるもののコメント投稿欄が表示されない' do
+    # トップページに移動する
+    visit root_path
+    # ツイートに「詳細」へのリンクがある
+    expect(
+      all(".more")[0].hover
+    ).to have_link '詳細', href: tweet_path(@tweet)
+    # 詳細ページに遷移する
+    visit tweet_path(@tweet)
+    # 詳細ページにツイートの内容が含まれている
+    expect(page).to have_selector ".content_post[style='background-image: url(#{@tweet.image});']"
+    expect(page).to have_content("#{@tweet.text}")
+    # フォームが存在しないことを確認する
+    expect(page).to have_no_selector 'form'
+    # 「コメントの投稿には新規登録/ログインが必要です」が表示されていることを確認する
+    expect(page).to have_content 'コメントの投稿には新規登録/ログインが必要です'
+  end
+end
