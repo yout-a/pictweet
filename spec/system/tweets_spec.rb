b2cff2d338456fb61e1e70ba816c24cca74a2452
yout-a <rainyday1388@gmail.com>
2025-09-14 13:23:15 +0900

    ツイートの投稿テスト

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -41,3 +41,67 @@ RSpec.describe 'ツイート投稿', type: :system do
     end
   end
 end
+
+RSpec.describe 'ツイート編集', type: :system do
+  before do
+    @tweet1 = FactoryBot.create(:tweet)
+    @tweet2 = FactoryBot.create(:tweet)
+  end
+    context 'ツイート編集ができるとき' do
+    it 'ログインしたユーザーは自分が投稿したツイートの編集ができる' do
+      # ツイート1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'Email', with: @tweet1.user.email
+      fill_in 'Password', with: @tweet1.user.password
+      find('input[name="commit"]').click
+      expect(page).to have_current_path(root_path)
+      # ツイート1に「編集」へのリンクがあることを確認する
+      expect(
+        all('.more')[1].hover
+      ).to have_link '編集', href: edit_tweet_path(@tweet1)
+      # 編集ページへ遷移する
+      visit edit_tweet_path(@tweet1)
+      # すでに投稿済みの内容がフォームに入っていることを確認する
+      expect(page).to have_field('tweet_image', with: @tweet1.image)
+      expect(page).to have_field('tweet_text', with: @tweet1.text)
+      # 投稿内容を編集する
+      fill_in 'tweet_image', with: "#{@tweet1.image}+編集した画像URL"
+      fill_in 'tweet_text', with: "#{@tweet1.text}+編集したテキスト"
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change { Tweet.count }.by(0)
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（画像）
+      expect(page).to have_selector ".content_post[style='background-image: url(#{@tweet1.image}+編集した画像URL);']"
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（テキスト）
+      expect(page).to have_content("#{@tweet1.text}+編集したテキスト")
+    end
+  end
+    context 'ツイート編集ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿したツイートの編集画面には遷移できない' do
+      # ツイート1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'Email', with: @tweet1.user.email
+      fill_in 'Password', with: @tweet1.user.password
+      find('input[name="commit"]').click
+      expect(page).to have_current_path(root_path)
+      # ツイート2に「編集」へのリンクがないことを確認する
+      expect(
+        all('.more')[0].hover
+      ).to have_no_link '編集', href: edit_tweet_path(@tweet2)
+    end
+    it 'ログインしていないとツイートの編集画面には遷移できない' do
+      # トップページにいる
+      visit root_path
+      # ツイート1に「編集」へのリンクがないことを確認する
+      expect(
+        all('.more')[1].hover
+      ).to have_no_link '編集', href: edit_tweet_path(@tweet1)
+      # ツイート2に「編集」へのリンクがないことを確認する
+      expect(
+        all('.more')[0].hover
+      ).to have_no_link '編集', href: edit_tweet_path(@tweet2)
+    end
+  end
+end
